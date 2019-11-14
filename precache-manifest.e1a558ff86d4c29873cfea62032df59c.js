2069feb97665f513851d4022259c3dcac79c70e7
Sergey Pushkovskiy <onyxpsi@ukr.net>
2019-11-14 08:27:59 +0200

    Updates

---
+++ precache-manifest.e1a558ff86d4c29873cfea62032df59c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef0b824633376b78fc395c00139c2a9f",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "a28b23d87bb15b4cd8d1",
+    "url": "/goit-react-hw-04-reader/static/css/main.3a4b4350.chunk.css"
+  },
+  {
+    "revision": "774b21d897e570f63c26",
+    "url": "/goit-react-hw-04-reader/static/js/2.1af80984.chunk.js"
+  },
+  {
+    "revision": "a28b23d87bb15b4cd8d1",
+    "url": "/goit-react-hw-04-reader/static/js/main.80379033.chunk.js"
+  },
+  {
+    "revision": "823667672d9a6097ea06",
+    "url": "/goit-react-hw-04-reader/static/js/runtime-main.3c80dfd2.js"
+  }
+]);
