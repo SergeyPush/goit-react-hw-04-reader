f202ddd591380d7581632d03eda71763ecd4b76d
Sergey Pushkovskiy <onyxpsi@ukr.net>
2019-11-14 08:25:08 +0200

    Added switch and redirect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import Reader from './components/Reader';
 import publications from './data/publications.json';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Route
-        path="/reader"
-        // eslint-disable-next-line react/jsx-props-no-spreading
-        render={props => <Reader {...props} items={publications} />}
-      />
-      <Route exact path="/">
+      <Switch>
+        <Route
+          path="/reader"
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          render={props => <Reader {...props} items={publications} />}
+        />
+        <Route exact path="/">
+          <Redirect to="/reader" />
+        </Route>
         <Redirect to="/reader" />
-      </Route>
+      </Switch>
     </BrowserRouter>
   );
 };
